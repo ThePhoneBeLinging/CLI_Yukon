7345218a21120a08ad7d1a76b4302aeae8d2cbbd
Elias Larsen <s235125@dtu.dk>
2024-04-14 14:52:56 +0200

    Created main.h and handleInput method

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Card.h"
-
+#include "main.h"
 int main (void)
 {
+    Pile* deck;
+    deck = malloc(sizeof (Pile));
     int AMOUNT_OF_COLOUMNS = 7;
     int AMOUNT_OF_FOUNDATIONS = 4;
     Pile* coloumns[AMOUNT_OF_COLOUMNS];
@@ -12,5 +14,15 @@ int main (void)
     mallocPileArr(coloumns,AMOUNT_OF_COLOUMNS);
 
     printf("Hello, World!\n");
+    handleInput();
     return 0;
+}
+
+void handleInput()
+{
+    char inputStr[50];
+    char raraStr[50];
+    printf("Board should be shown and show last message and output \n");
+    scanf("%[^\n]s",inputStr);
+    printf("%s",inputStr);
 }
