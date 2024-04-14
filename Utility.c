0cf2e29e0379b3d335d973d9e2f2af77f900c40c
Elias Larsen <s235125@dtu.dk>
2024-04-14 22:49:48 +0200

    Created utility c and header file, with function for comparing 2 strings

---
+++ Utility.c
@@ -0,0 +1,5 @@
+#include <string.h>
+bool stringsAreEqual(char* firstString, char* secondString)
+{
+    return strcmp(firstString,secondString) == 0;
+}
---
+++ Utility.h
@@ -0,0 +1,1 @@
+bool stringsAreEqual(char*, char*);
